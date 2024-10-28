3857cd7651074751f1ece77ad61bad6edc476811
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-10-28 09:22:11 +0000

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
 import './../styles/App.css';
+import DynamicForm from "./DynamicForm";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <DynamicForm />
     </div>
   )
 }
 
-export default App
+export default App;
