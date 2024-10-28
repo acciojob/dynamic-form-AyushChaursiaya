459331516ac771c61d139a7a08399bb956356b8d
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-10-28 09:58:12 +0000

    add

--- src/components/DynamicForm.js
+++ src/components/DynamicForm.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import '../styles/App.css';
 
 const DynamicForm = () => {
     const [fields, setFields] = useState([]);
 
     const handleAdd = () => {
-        const newField = {id: Date.now(), value: ''};
+        const newField = { id: Date.now(), value: '' };
         setFields([...fields, newField]);
         // console.log(newField);
         // console.log('field =>', fields);
@@ -18,25 +19,28 @@ const DynamicForm = () => {
         const updateInput = fields.map((field) => {
             if (field.id === id) {
                 return { ...field, value: value };
-                }else {
-                    return field;
-                }
+            } else {
+                return field;
+            }
         })
         setFields(updateInput);
     }
 
     return (<>
-    <div>
-        {
-            fields.map((field) => (
-                <div key={field.id}>
-                    <input style={{margin: '5px'}} type='text' placeholder='Enter Name' value={fields.value} onClick={(e) => handleInput(field.id , e.target.value)} />
-                    <button style={{border: 'none', background: 'blue', color: '#ffffff', borderRadius: '4px', width: '100px', height: '25px'}} onClick={() => handleRemove(field.id)}>Delete</button>
-                </div>
-            ))
-        }
-    </div>
-    <button style={{border: 'none', background: 'blue', color: '#ffffff', borderRadius: '4px', width: '100px', height: '25px'}} onClick={handleAdd}>Add Field</button>
+        <div>
+            {
+                fields.length === 0 ? (<p>No fields in the form</p>) :
+                    (
+                        fields.map((field) => (
+                            <div key={field.id}>
+                                <input style={{ margin: '5px' }} type='text' placeholder='Enter Name' value={fields.value} onClick={(e) => handleInput(field.id, e.target.value)} />
+                                <button className='delete' onClick={() => handleRemove(field.id)}>Delete</button>
+                            </div>
+                        ))
+                    )
+            }
+        </div>
+        <button className='add' onClick={handleAdd}>Add Field</button>
     </>)
 }
 
